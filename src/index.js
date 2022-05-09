737b1ef3cd823558e80263e5ab56e7054cfbc58e
Edgar Pureco <pureco.edgar.1am@gmail.com>
2022-05-09 17:27:59 +0200

    profile_commit

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import SignUp from './pages/SignUp';
 import GetUser from './pages/admin/GetUser';
 import UpdateGame from './pages/admin/UpdateGame';
 import Cart from './pages/Cart';
+import Profile from './pages/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -27,6 +28,7 @@ root.render(
         <Route path="/logout" exact element={<LogOut/>}  />    
         <Route path="/signup" exact element={<SignUp/>}  />   
         <Route path="/cart" exact element={<Cart/>}  />    
+        <Route path="/profile" exact element={<Profile />}  />   
         <Route path="/category/:category" exact element={<Category />}  />    
         <Route path="/addGame" exact element={<AddGame />}  />
         <Route path="/addCategory" exact element={<AddCategory />}  /> 
